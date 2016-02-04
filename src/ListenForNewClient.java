3ecabcfc34040438f0613900f6c1e4fc67a7e6ad
yitzih <yitzchakhorowitz@gmail.com>
2016-02-04 04:26:38 -0500

    added code to allow server to continuously listen for new clients trying to connect

--- src/ListenForNewClient.java
+++ src/ListenForNewClient.java
@@ -1,9 +1,14 @@
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Listens for new connection requests
  */
 public class ListenForNewClient extends Thread {
 
-    Server server;
+    private Server server;
+
+    private boolean lookForNewClients = true;
 
     ListenForNewClient(Server server) {
         this.server = server;
@@ -12,6 +17,36 @@ public class ListenForNewClient extends Thread {
     @Override
     public void run(){
 
+        Socket socket = null;
+
+        /*
+         * continuous loop to accept new clients
+         */
+        while (lookForNewClients) {
+
+            //get socket connecting to the new client
+            try {
+                socket = getNewClientSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //create a new ClientConnectionData to hold the necessary fields for
+            //getting and sending information from and to the client
+            ClientConnectionData ccd = new ClientConnectionData(socket);
+
+            //store the new ClientConnectionData in the server
+            server.storeNewClientConnectionData(ccd);
+        }
+
     }
 
+    /**
+     * Gets the socket connecting to a client
+     *
+     * @return socket connecting the server to the client
+     */
+    private Socket getNewClientSocket() throws IOException {
+        return server.getNewClientSocket();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Chat server manages all communication between clients
@@ -9,7 +12,10 @@ public class Server {
     public static final int PORT = 22222;
     public static final int BACKLOG = 15;
 
-    ServerSocket serverSocket;
+    private ServerSocket serverSocket;
+
+    //holds all the connection data for clients connected to the server
+    private List<ClientConnectionData> clients = new ArrayList<ClientConnectionData>();
 
     /**
      * constructor
@@ -30,6 +36,24 @@ public class Server {
      */
     private void startListeningForNewClients() {
         new ListenForNewClient(this).start();
+    }
+
+    /**
+     * Get the socket after accepting the clients connection request
+     *
+     * @return socket from new client requesting connection
+     */
+    public Socket getNewClientSocket() throws IOException {
+        return this.serverSocket.accept();
+    }
+
+    /**
+     * Stores the client connection data in the servers list of clients
+     *
+     * @param ccd The new client being saved
+     */
+    public void storeNewClientConnectionData(ClientConnectionData ccd) {
+        this.clients.add(ccd);
     }
 
 }
