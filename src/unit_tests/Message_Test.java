2c61254ad97f58853e6bb78735c34551dcd102e4
yitzih <yitzchakhorowitz@gmail.com>
2016-02-16 10:42:11 -0500

    created package for unit tests

---
+++ src/unit_tests/Message_Test.java
@@ -0,0 +1,15 @@
+package unit_tests;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for the Message Class
+ */
+public class Message_Test {
+
+    @Test
+    public void testGetterAndSetterForUserId() {
+
+    }
+
+}
