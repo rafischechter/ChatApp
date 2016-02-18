5efb064182471a977f3fe3c20b0f69d0dcf1e2c7
yitzih <yitzchakhorowitz@gmail.com>
2016-02-18 08:18:01 -0500

    removed tests that couldnt be tested and moved the test methods to reflect the ordering in the Message class

--- src/unit_tests/Message_Test.java
+++ src/unit_tests/Message_Test.java
@@ -15,12 +15,6 @@ import static org.junit.Assert.*;
  */
 public class Message_Test {
 
-    @Test
-    public void testSetAndGetSetText() throws Exception {
-        Message m = new Message();
-        m.setText("Hello World");
-        assertEquals("testing setter and getter for text", "Hello World", m.getText());
-    }
 
     @Test
     public void testSetAndGetUserId() throws Exception {
@@ -29,48 +23,29 @@ public class Message_Test {
         assertEquals("testing setter and getter for userId", "5", m.getUserId());
     }
 
+    /*
+        The getter and setter for the timestamp can't be tested
+        because it works based off the current time that the Message is created
+    */
 
     @Test
-    public void testSetAndGetTimeStamp() throws Exception {
-        /*
-            can't be tested as it works based off the current time that
-            the Message is created
-         */
+    public void testSetAndGetSetText() throws Exception {
+        Message m = new Message();
+        m.setText("Hello World");
+        assertEquals("testing setter and getter for text", "Hello World", m.getText());
     }
 
+    /*
+        The getCurrentDate() method doesn't need to be tested because it
+        relies on the testing of the Date class
+     */
 
-    @Test
-    public void testSetAndGetImage() throws Exception {
-        /*
-            unsure how to test beyond checking if the variable is not null
-            which is tested in another test
-         */
-    }
+    /*
+        The getFormattedTimeStamp() method can't be tested because it uses the
+        timeStamp variable is only set upon creation. It relies on the testing of the
+        SimpleDateFormat class
+     */
 
-    @Test
-    public void testSetAndGetFile() throws Exception {
-        /*
-            unsure how to test beyond checking if the variable is not null
-            which is tested in another test
-         */
-    }
-
-    @Test
-    public void testGetCurrentDate() throws Exception {
-        /*
-         * No test that can currently be run on this method
-         *
-         * It created a new Date object from the Date class
-         */
-    }
-
-    @Test
-    public void tesGetFormattedTimeStamp() throws Exception {
-        /*
-            No way to test this method without being able to
-            test getCurrentDate
-         */
-    }
 
     @Test
     public void testHasText() throws Exception {
@@ -94,6 +69,9 @@ public class Message_Test {
     }
 
     @Test
+    /*
+        This also tests the getImage and setImage methods
+     */
     public void testHasImage() throws Exception {
         Message m = new Message();
         assertEquals("test message has no image immediately after creation", false, m.hasImage());
@@ -104,6 +82,9 @@ public class Message_Test {
     }
 
     @Test
+    /*
+        This also tests the getFile and setFile methods
+     */
     public void testHasFile() throws Exception {
         Message m = new Message();
         assertEquals("test message has no file immediately after creation", false, m.hasFile());
